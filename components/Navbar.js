c1f634c91065ccc20a2527a5bcbb9a10aee9241e
rahulkolhe <rahulkolhe90.rk.rk@gmail.com>
2023-02-21 14:44:27 +0530

    productpage create

--- components/Navbar.js
+++ components/Navbar.js
@@ -74,7 +74,12 @@ const toggleCart = ()=>{
         </div>
     </li>
 </ol>
-    <button className="flex  mt-16 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded text-sm"><BsFillBagCheckFill className='m-1'/>Checkout</button>
+<div className="flex">
+<button className="flex mr-2  text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded text-sm"><BsFillBagCheckFill className='m-1'/>Checkout</button>
+
+    <button className="flex mr-2 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-sm">ClearCart</button>
+</div>
+    
 
     </div>
     </div>
