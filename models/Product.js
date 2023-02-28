889e7006fd14990700f213286c01d8bfa6c6a308
rahulkolhe <rahulkolhe90.rk.rk@gmail.com>
2023-02-28 15:26:28 +0530

    get tshirt product from Database and show on Screen

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,17 @@
-
-
 const mongoose = require("mongoose");
-
+// https://m.media-amazon.com/images/I/81nsMsrRB4L._AC_UL1500_.jpg
+// https://m.media-amazon.com/images/I/71wPoz1vxpL._AC_UL1500_.jpg
+// https://m.media-amazon.com/images/I/81JY6XT8pGL._AC_UL1500_.jpg
+// https://m.media-amazon.com/images/I/61RdXJNMRXL._AC_UL1010_.jpg
 const productSchema = new mongoose.Schema(
   {
+    // unique: true
     title: { type: String, required: true },
-    slug: { type: String, required: true, unique: true },
-    desc: { type: String, required: true, unique: true },
-    img: { type: String, required: true, unique: true },
-    category: { type: String, required: true, unique: true }, // kahase bolong karata ha tshirt , mugs, hoodies tyasathi category
-    size: { type: String },
+    slug: { type: String, required: true},
+    desc: { type: String, required: true },
+    img: { type: String, required: true },
+    category: { type: String }, // kahase bolong karata ha tshirt , mugs, hoodies tyasathi category
+    size: { type: String, required: true},
     color: { type: String },
     price: { type: Number, required: true },
     availableQty: { type: Number, required: true },
@@ -17,5 +19,5 @@ const productSchema = new mongoose.Schema(
   { timestamps: true }
 ); // created and updated at property create karnyasathi mongoose schema cha second argument use karu shakta {timestamp: true} ani ha ek object asnar ahe tyamule{}
 
-mongoose.models = {} // cannot overwrite product model ha error solve jevha import hoyil punha bnvnyacha try krto tymule error yeto
-export default mongoose.model("Product", productSchema) // productSchema ne Schema bnel table bnel ani ya table ch nav asel Order
+mongoose.models = {}; // cannot overwrite product model ha error solve jevha import hoyil punha bnvnyacha try krto tymule error yeto
+export default mongoose.model("Product", productSchema); // productSchema ne Schema bnel table bnel ani ya table ch nav asel Order
--- pages/tshirt.js
+++ pages/tshirt.js
@@ -1,134 +1,61 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import Product from "@/models/Product";
+// import connectDb from "@/middleware/mongoose";
+import mongoose from "mongoose";
 
-const Tshirt = () => {
+const Tshirt = ({ product }) => {
   return (
     <div>
-    <section className="text-gray-600 body-font">
-  <div className="container px-5 py-24 mx-auto">
-    <div className="flex flex-wrap -m-4 justify-center">
-       <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-2 ">
- <Link href={'/product/hello'}>
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._SX569._SX._UX._SY._UY_.jpg"></img>
-        </a>
-        <div className="mt-4 text-center">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-          <p className="mt-1">₹16.00</p>
-                    <p className="mt-1">S, M,L,XL</p>
-
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-wrap  -m-4 justify-center ">
+            {product.map((item) => {
+              return (
+                <Link
+                  passHref={true}
+                  href={`/product/${item.slug}`}
+                  key={item._id}
+                  className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-2"
+                >
+                  <a className="block relative rounded overflow-hidden">
+                    <img
+                      alt="ecommerce"
+                      className="m-auto h-[30vh] md:h-[36vh] block"
+                      src={item.img}
+                    ></img>
+                  </a>
+                  <div className="mt-4 text-center">
+                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+                      Tshirt
+                    </h3>
+                    <h2 className="text-gray-900 title-font text-lg font-medium">
+                      {item.title}
+                    </h2>
+                    <p className="mt-1">₹{item.price}</p>
+                    <p className="mt-1">{item.size}</p>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
         </div>
-      </Link>
-  
-      </div>
-      <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-2">
-         <Link href={'/product/hello'}>
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto  h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._SX569._SX._UX._SY._UY_.jpg"/>
-        </a>
-        <div className="mt-4 text-center">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-          <p className="mt-1">₹21.15</p>
-          <p className="mt-1">S, M,L,XL</p>
-        </div>
-        </Link>
-      </div> 
-      
- 
-      <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-2">
-          <Link href={'/product/hello'}>
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className=" m-auto  h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._SX569._SX._UX._SY._UY_.jpg"></img>
-        </a>
-        <div className="mt-4 text-center">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-          <p className="mt-1">₹12.00</p>
-          <p className="mt-1">S, M,L,XL</p>
-        </div>
-        </Link>
-      </div>
-      <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-2">
-         <Link href={'/product/hello'}>
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto  h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._SX569._SX._UX._SY._UY_.jpg"></img>
-        </a>
-        <div className="mt-4 text-center">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-          <p className="mt-1">₹18.40</p>
-          <p className="mt-1">S, M,L,XL</p>
-
-        </div>
-        </Link>
-      </div>
-     
-      <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-2">
-       <Link href={'/product/hello'}>
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto  h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._SX569._SX._UX._SY._UY_.jpg"></img>
-        </a>
-        <div className="mt-4 text-center">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-          <p className="mt-1">₹16.00</p>
-          <p className="mt-1">S, M,L,XL</p>
-        </div>
-        </Link>
-      </div>
-     
-      <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-2">
-          <Link href={'/product/hello'}>
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block"  src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._SX569._SX._UX._SY._UY_.jpg"></img>
-        </a>
-        <div className="mt-4 text-center">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Shooting Stars</h2>
-          <p className="mt-1">₹21.15</p>
-          <p className="mt-1">S, M,L,XL</p>
-
-        </div>
-        </Link>
-      </div>
-    
-      <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-2">
-                  <Link href={'/product/hello'}>
-
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto  h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._SX569._SX._UX._SY._UY_.jpg"></img>
-        </a>
-        <div className="mt-4 text-center">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">Neptune</h2>
-          <p className="mt-1">₹12.00</p>
-          <p className="mt-1">S, M,L,XL</p>
-
-        </div>
-        </Link>
-      </div>
-
-      <div className="lg:w-1/5 md:w-1/2 p-4 w-full shadow-lg m-2">
-         <Link href={'/product/hello'}>
-        <a className="block relative rounded overflow-hidden">
-          <img alt="ecommerce" className="m-auto h-[30vh] md:h-[36vh] block" src="https://m.media-amazon.com/images/I/61Mw4M+mHwL._SX569._SX._UX._SY._UY_.jpg"/>
-        </a>
-        <div className="mt-4 text-center">
-          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">Tshirt</h3>
-          <h2 className="text-gray-900 title-font text-lg font-medium">The 400 Blows</h2>
-          <p className="mt-1">₹18.40</p>
-          
-        </div>
-        </Link>
-      </div>
-    
+      </section>
     </div>
-  </div>
-</section>
-    </div>
-  )
+  );
+};
+
+//-----------serverSideProps-------------
+export async function getServerSideProps(context) {
+  if (!mongoose.connections[0].readyState) {
+    //jitne bhi mongoose connection hai unka pahila connection readyState hai mtalb already
+    await mongoose.connect(process.env.MONGO_URI);
+  }
+
+  let product = await Product.find({ category: "tshirt" });  //product table madhun category: tshirt asel tech get hotil karan product ya tabel madhe khup sare product mnje item astil jyanchi category tshirt asel tech get hotil
+  return {
+    props: { product: JSON.parse(JSON.stringify(product)) }, //props send kela ya file chya function la
+  };
 }
 
-export default Tshirt
+export default Tshirt;
